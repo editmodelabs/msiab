541e51c3d0335b8523ae8b7830bfd8f3fde0f8f4
Francis Bulus <notchera@gmail.com>
2021-05-26 17:06:38 +0100

    add features section

--- themes/lagos/pages/index.js
+++ themes/lagos/pages/index.js
@@ -1,4 +1,4 @@
-import Hero from "../components/Hero";
+import Hero from "../sections/Hero";
 
 export default function Home() {
   return (
---
+++ themes/lagos/sections/Features.jsx
@@ -0,0 +1,173 @@
+export default function Features() {
+  return (
+    <section>
+      <div class="skew skew-top mr-for-radius">
+        <svg
+          class="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+          viewBox="0 0 10 10"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="currentColor" points="0 0 10 10 0 10" />
+        </svg>
+      </div>
+      <div class="skew skew-top ml-for-radius">
+        <svg
+          class="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+          viewBox="0 0 10 10"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="currentColor" points="0 10 10 0 10 10" />
+        </svg>
+      </div>
+      <div class="py-20 bg-gray-50 radius-for-skewed">
+        <div class="container mx-auto px-4">
+          <div class="mb-16 max-w-md mx-auto text-center">
+            <span class="text-green-600 font-bold">
+              Dolor sit amet consectutar
+            </span>
+            <h2 class="text-4xl lg:text-5xl font-bold font-heading">
+              Build &amp; Launch without problems
+            </h2>
+          </div>
+          <div class="flex flex-wrap -mx-3">
+            <div class="mb-6 w-full lg:w-1/2 px-3">
+              <div class="p-6 flex flex-wrap bg-white shadow rounded-lg">
+                <div>
+                  <span class="mb-4 lg:mb-0 mr-6 inline-block p-3 md:p-5 rounded-lg bg-green-100">
+                    <svg
+                      class="h-8 w-8 md:w-12 md:h-12 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewbox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="w-full lg:w-2/3">
+                  <h3 class="mb-2 text-2xl font-bold font-heading">
+                    Ut congue nec leo eget
+                  </h3>
+                  <p class="text-gray-500">
+                    Donec ut ligula nunc. Mauris blandit vel est et facilisis.
+                    Integer sapien felis, aliquet.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mb-6 w-full lg:w-1/2 px-3">
+              <div class="p-6 flex flex-wrap bg-white shadow rounded-lg">
+                <div>
+                  <span class="mb-4 lg:mb-0 mr-6 inline-block p-3 md:p-5 rounded-lg bg-green-100">
+                    <svg
+                      class="h-8 w-8 md:w-12 md:h-12 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewbox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="w-full lg:w-2/3">
+                  <h3 class="mb-2 text-2xl font-bold font-heading">
+                    Lorem ipsum dolor sit amet
+                  </h3>
+                  <p class="text-gray-500">
+                    Donec ut ligula nunc. Mauris blandit vel est et facilisis.
+                    Integer sapien felis, aliquet.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mb-6 lg:mb-0 w-full lg:w-1/2 px-3">
+              <div class="p-6 flex flex-wrap bg-white shadow rounded-lg">
+                <div>
+                  <span class="mb-4 lg:mb-0 mr-6 inline-block p-3 md:p-5 rounded bg-green-100">
+                    <svg
+                      class="h-8 w-8 md:w-12 md:h-12 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewbox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="w-full lg:w-2/3">
+                  <h3 class="mb-2 text-2xl font-bold font-heading">
+                    Morbi sagittis ligula sit
+                  </h3>
+                  <p class="text-gray-500">
+                    Donec ut ligula nunc. Mauris blandit vel est et facilisis.
+                    Integer sapien felis, aliquet.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/2 px-3">
+              <div class="p-6 flex flex-wrap bg-white shadow rounded-lg">
+                <div>
+                  <span class="mb-4 lg:mb-0 mr-6 inline-block p-3 md:p-5 rounded bg-green-100">
+                    <svg
+                      class="h-8 w-8 md:w-12 md:h-12 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewbox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                        clip-rule="evenodd"
+                      ></path>
+                      <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="w-full lg:w-2/3">
+                  <h3 class="mb-2 text-2xl font-bold font-heading">
+                    Proin fringilla eleifend justo
+                  </h3>
+                  <p class="text-gray-500">
+                    Donec ut ligula nunc. Mauris blandit vel est et facilisis.
+                    Integer sapien felis, aliquet.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="skew skew-bottom mr-for-radius">
+        <svg
+          class="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+          viewBox="0 0 10 10"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="currentColor" points="0 0 10 0 0 10" />
+        </svg>
+      </div>
+      <div class="skew skew-bottom ml-for-radius">
+        <svg
+          class="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+          viewBox="0 0 10 10"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="currentColor" points="0 0 10 0 10 10" />
+        </svg>
+      </div>
+    </section>
+  );
+}
