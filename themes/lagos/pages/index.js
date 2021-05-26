00b64fd681057cd780c88fb16d8510bad0d142b2
Francis Bulus <notchera@gmail.com>
2021-05-26 16:56:12 +0100

    reverse hero decoupling

--- themes/lagos/pages/index.js
+++ themes/lagos/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Hero from "../sections/Hero";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
--- themes/lagos/sections/Hero.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import NavBar from "../components/NavBar";
-
-export default function Nav() {
-  return <NavBar />;
-}
