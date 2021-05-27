1fbfa50be08b2c9ee454c0d77a860f8e8086aaff
Francis Bulus <notchera@gmail.com>
2021-05-27 22:25:27 +0100

    add additional features section content

--- themes/lagos/pages/index.js
+++ themes/lagos/pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "../sections/Hero";
+import Features from "../sections/Features";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Hero />
-    </>
+      <Features />
+    </div>
   );
 }
