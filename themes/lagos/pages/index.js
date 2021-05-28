573021bc698ff962b391d1cf835dcfbf9294ed45
Francis Bulus <notchera@gmail.com>
2021-05-28 05:45:24 +0100

    expanding sections file tree structure

--- themes/lagos/pages/index.js
+++ themes/lagos/pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../sections/Hero";
 import Features from "../sections/Features";
+import Team from "../sections/Team";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      <Team />
     </div>
   );
 }
