20492e564bcb1f835bc7744c98f2fea1c64d67eb
Francis Bulus <notchera@gmail.com>
2021-05-31 21:12:17 +0100

    add assets

--- themes/lagos/components/FaqTabs.js
+++ themes/lagos/components/FaqTabs.js
@@ -1,25 +1,9 @@
 import Panel from "./Panel";
 import useTabState from "../utils/useTabState";
 import Tabs from "./Tabs";
-import computeClassName from "../utils/computeClassName";
-import General from "./GeneralTab";
-import Payment from "./PaymentsTab";
-
-const cn = (...args) => args.filter(Boolean).join(" ");
-
-export const Tab = ({ children }) => {
-  const { isActive, onClick } = useTabState();
-  const add_cn = computeClassName(isActive);
-
-  return (
-    <button
-      className={cn("tab", isActive && "active") + add_cn}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+import Tab from "./Tab";
+import GeneralPanelContent from "./GeneralPanel";
+import PaymentPanelContent from "./PaymentsPanel";
 
 export default function FaqTabs() {
   return (
@@ -36,7 +20,6 @@ export default function FaqTabs() {
         <div class="max-w-3xl mx-auto">
           <div class="flex flex-wrap px-4 text-center lg:-mx-4 lg:space-x-4 text-base lg:text-xl">
             <Tab>General</Tab>
-
             <Tab>Payments</Tab>
           </div>
         </div>
@@ -45,11 +28,11 @@ export default function FaqTabs() {
       <div className="tab-progress" />
 
       <Panel>
-        <General />
+        <GeneralPanelContent />
       </Panel>
 
       <Panel>
-        <Payment />
+        <PaymentPanelContent />
       </Panel>
     </Tabs>
   );
