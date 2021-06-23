b9206f3e65e358602b2fb60f32862dfe48061606
Francis Bulus <notchera@gmail.com>
2021-06-23 04:45:16 +0100

    comment out purge config to test css exclusion solution in production

--- themes/lagos/tailwind.config.js
+++ themes/lagos/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  // eslint-disable-next-line global-require
+};
