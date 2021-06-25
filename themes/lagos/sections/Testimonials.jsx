6aa8ebb3ac9211d4c98666ddac3a733dccd9febb
Francis Bulus <notchera@gmail.com>
2021-06-25 09:18:28 +0100

    take out redundant svg

--- themes/lagos/sections/Testimonials.jsx
+++ themes/lagos/sections/Testimonials.jsx
@@ -8,15 +8,6 @@ import {
 export default function Testimonials() {
   return (
     <section>
-      <div className="skew skew-top mr-for-radius">
-        <svg
-          className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
-          viewBox="0 0 10 10"
-          preserveAspectRatio="none"
-        >
-          <polygon fill="currentColor" points="0 0 10 10 0 10" />
-        </svg>
-      </div>
       <div className="skew skew-top ml-for-radius">
         <svg
           className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
