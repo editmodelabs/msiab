e549921eb3e6ca08621c84da7d4d30fe5c1e28f0
Francis Bulus <notchera@gmail.com>
2021-06-25 10:14:12 +0100

    reset to head~1 and redo fix for styling bug that messses up svg skews

--- themes/lagos/pages/_app.js
+++ themes/lagos/pages/_app.js
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import "../styles/globals.min.css";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
