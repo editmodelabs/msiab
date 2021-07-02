768634e96be812c391c4b07ce63a8034dfdbeaf6
tonyennis145 <ennis.tony@gmail.com>
2021-07-02 14:59:34 +0800

    Convert pricing page
    Still to complete description section

--- themes/lagos/pages/pricing.js
+++ themes/lagos/pages/pricing.js
@@ -2,6 +2,7 @@
 import Footer from "../sections/Footer";
 import TopNav from "../sections/TopNav";
 import Head from 'next/head'
+import { Chunk, useGetChunk, ChunkCollection } from "editmode-react";
 
 export default function Pricing() {
   return (
@@ -24,109 +25,64 @@ export default function Pricing() {
         <div class="py-20 bg-gray-50 radius-for-skewed">
           <div class="container mx-auto px-4">
             <div class="mb-16 w-full flex flex-wrap items-center">
-              <div class="w-full lg:w-1/2">
-                <span class="text-primary font-bold">Dolor sit amet consectutar</span>
-                <h2 class="mb-2 text-4xl lg:text-5xl font-bold font-heading">Choose your best plan</h2>
+              <div class="w-full lg:w-2/3">
+                <span class="text-primary font-bold">
+                  <Chunk identifier='pricing_page_tagline' />
+                </span>
+                <h2 class="mb-2 text-4xl lg:text-5xl font-bold font-heading">
+                  <Chunk identifier='pricing_page_headline' />
+                </h2>
               </div>
-              <div class="w-full lg:w-1/2">
-                <p class="max-w-xs lg:mx-auto text-gray-500 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur nisl sodales egestas lobortis.</p>
-              </div>
-            </div>
-            <div class="flex flex-wrap">
-              <div class="mb-8 w-full p-8 flex flex-wrap items-center bg-white rounded shadow">
-                <div class="w-full lg:w-1/5 px-3 self-start">
-                  <h3 class="mb-4 text-2xl font-bold font-heading">Beginner</h3>
-                </div>
-                <div class="w-full lg:w-2/5 px-3">
-                  <ul class="mb-4 text-gray-500">
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Vestibulum viverra</span>
-                    </li>
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Morbi mollis metus pretium</span>
-                    </li>
-                    <li class="flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Etiam lectus nunc, commodo</span>
-                    </li>
-                  </ul>
-                </div>
-                <div class="w-full lg:w-1/5 px-3 lg:text-center">
-                  <span class="text-4xl font-bold">Free</span>
-                </div>
-                <div class="w-full lg:w-1/5 px-3"><a class="inline-block mt-4 lg:mt-0 py-2 px-6 rounded-l-xl rounded-t-xl bg-primary hover:bg-green-700 text-white font-bold leading-loose transition duration-200" href="#">Get Started</a></div>
-              </div>
-              <div class="mb-8 w-full p-8 flex flex-wrap items-center bg-white rounded shadow">
-                <div class="w-full lg:w-1/5 px-3 self-start">
-                  <h3 class="mb-4 text-2xl font-bold font-heading">Intermediate</h3>
-                </div>
-                <div class="w-full lg:w-2/5 px-3">
-                  <ul class="mb-4 text-gray-500">
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Vestibulum viverra</span>
-                    </li>
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Morbi mollis metus pretium</span>
-                    </li>
-                    <li class="flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Etiam lectus nunc, commodo</span>
-                    </li>
-                  </ul>
-                </div>
-                <div class="w-full lg:w-1/5 px-3 lg:text-center">
-                  <span class="text-4xl font-bold">$29</span>
-                </div>
-                <div class="w-full lg:w-1/5 px-3"><a class="inline-block mt-4 lg:mt-0 py-2 px-6 rounded-l-xl rounded-t-xl bg-primary hover:bg-green-700 text-white font-bold leading-loose transition duration-200" href="#">Get Started</a></div>
-              </div>
-              <div class="w-full p-8 flex flex-wrap items-center bg-white rounded shadow">
-                <div class="w-full lg:w-1/5 px-3 self-start">
-                  <h3 class="mb-4 text-2xl font-bold font-heading">Professional</h3>
-                </div>
-                <div class="w-full lg:w-2/5 px-3">
-                  <ul class="mb-4 text-gray-500">
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Vestibulum viverra</span>
-                    </li>
-                    <li class="mb-4 flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Morbi mollis metus pretium</span>
-                    </li>
-                    <li class="flex">
-                      <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                      </svg>
-                      <span>Etiam lectus nunc, commodo</span>
-                    </li>
-                  </ul>
-                </div>
-                <div class="w-full lg:w-1/5 px-3 lg:text-center">
-                  <span class="text-4xl font-bold">$49</span>
-                </div>
-                <div class="w-full lg:w-1/5 px-3"><a class="inline-block mt-4 lg:mt-0 py-2 px-6 rounded-l-xl rounded-t-xl bg-primary hover:bg-green-700 text-white font-bold leading-loose transition duration-200" href="#">Get Started</a></div>
+              <div class="w-full lg:w-1/3 text-gray-600">
+                <Chunk identifier='pricing_page_description' />
               </div>
             </div>
+            <ChunkCollection identifier="pricing_packages" className="flex flex-wrap" itemClass="mb-8 w-full p-8 flex flex-wrap items-center bg-white rounded shadow" >
+
+              {(getChunk, chunk) => (
+                <>
+                  <div class="w-full lg:w-1/5 px-3 self-start">
+                    <h3 class="mb-4 text-2xl font-bold font-heading">
+                      {getChunk(chunk, "Title")}
+                    </h3>
+                  </div>
+                  <div class="w-full lg:w-2/5 px-3">
+                    <ul class="mb-4 text-gray-500">
+                      <li class="mb-4 flex">
+
+                        <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span>Vestibulum viverra</span>
+                      </li>
+                      <li class="mb-4 flex">
+                        <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span>Morbi mollis metus pretium</span>
+                      </li>
+                      <li class="flex">
+                        <svg class="mr-2 w-5 h-5 text-primary" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span>Etiam lectus nunc, commodo</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="w-full lg:w-1/5 px-3 lg:text-center">
+                    <div class="text-4xl font-bold">
+                      {getChunk(chunk, "Price")}
+                    </div>
+                  </div>
+                  <div class="w-full lg:w-1/5 px-3">
+                    <a class="inline-block mt-4 lg:mt-0 py-2 px-6 rounded-l-xl rounded-t-xl bg-primary hover:bg-green-700 text-white font-bold leading-loose transition duration-200" href={getChunk(chunk,"Link")}>
+                      {getChunk(chunk,"Button Text")}
+                    </a>
+                  </div>
+                </>
+              )}
+
+            </ChunkCollection>
           </div>
         </div>
         <div class="skew skew-bottom mr-for-radius">
