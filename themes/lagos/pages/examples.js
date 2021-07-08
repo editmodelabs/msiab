183585fa0f5bca85ec8118ac8dd660a363a8d11a
tonyennis145 <ennis.tony@gmail.com>
2021-07-08 09:47:41 +0800

    Add examples page

---
+++ themes/lagos/pages/examples.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import {Editmode, Chunk, ChunkCollection, ChunkFieldValue, useGetChunk} from "editmode-react"
+
+export default function Home() {
+  return (
+    <Editmode projectId="prj_Y5HfCBS4rqZg">
+      
+      <div className="container mx-auto">
+
+        {/* Render a piece of standalone content with inline editing */}
+        <Chunk identifier="logo_icon" className="w-5 text-primary" />  {/* Image Chunk */}
+        <Chunk identifier="company_name" /> {/* Plain Text Chunk */}
+        
+        {/* Render content from a collection, with inline editing */}
+        <ChunkCollection identifier="testimonials" >   
+          <ChunkFieldValue identifier="Name" />  
+          <ChunkFieldValue identifier="Role" />   
+          <ChunkFieldValue identifier="Comment" />
+        </ChunkCollection>
+
+        {/* Access the raw value of a chunk. (No inline editing)  */}
+        <Head>
+          <title>{useGetChunk("company_tagline")}</title>
+        </Head>
+
+      </div>
+      
+    </Editmode>
+  )
+}
