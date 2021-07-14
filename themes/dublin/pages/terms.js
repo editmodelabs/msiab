1c84dda7daa8ba4ec350b791ecc94ae9aa7af453
Megan Ennis <mennis0145@gmail.com>
2021-07-14 17:48:58 +0100

    Add terms & conditions static page

---
+++ themes/dublin/pages/terms.js
@@ -0,0 +1,22 @@
+import {Chunk} from "editmode-react"
+import Layout from "../components/Layout";
+
+export default function Terms() {
+  return (
+    <section className="pb-12 lg:pb-24 pt-12 lg:pt-20 overflow-hidden bg-gray-100 min-h-screen">
+      <div className="container px-4 mx-auto">
+        <div className="rounded bg-white shadow px-12 py-10 text-center">
+          <h1 className="text-xl lg:text-3xl font-bold mb-10">
+            <Chunk contentKey="terms_conditions_static_page" field="Title"/>
+          </h1>
+          <div>
+            <Chunk contentKey="terms_conditions_static_page" field="Body"/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Terms.getLayout = (page) => <Layout pageName="Terms">
+  {page}</Layout>;
