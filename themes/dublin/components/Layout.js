ca0144cd8f1496e33539c4d62f621a662e1600b9
Megan Ennis <mennis0145@gmail.com>
2021-07-14 17:06:13 +0100

    Seperate global styles into own component. Wrap layout in Editmode wrapper.

--- themes/dublin/components/Layout.js
+++ themes/dublin/components/Layout.js
@@ -1,35 +1,31 @@
-import Head from 'next/head'
-import { useGetChunk } from "editmode-react";
+import Head from "next/head";
+import Styles from "./Styles";
+import TopNavbar from "../sections/TopNavbar";
+import Footer from "../sections/Footer";
+import { useGetChunk, Editmode } from "editmode-react";
+import handleNav from "../utils/handleNav";
+import { useEffect } from "react";
 
 function Layout(props) {
-  const primaryColor = useGetChunk("primary_color")
-  const font = useGetChunk('google_font_family')
-  const defaultMetaTitle = `${useGetChunk('company_name')} -  ${useGetChunk('company_tagline')}`
+  const defaultMetaTitle = `${useGetChunk("company_name")} -  ${useGetChunk(
+    "company_tagline"
+  )}`;
+  useEffect(() => {
+    handleNav();
+  }, []);
 
   return (
-    <div>
+    <Editmode projectId={process.env.NEXT_PUBLIC_PROJECT_ID}>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>{defaultMetaTitle}</title>
       </Head>
+      <Styles />
+      <TopNavbar />
       {props.children}
-      <style jsx global>{`
-        @import url('https://fonts.googleapis.com/css2?family=${font}&display=swap');
-        .text-primary {
-          color: ${primaryColor};
-        }
-        .bg-primary {
-          background-color: ${primaryColor};
-        }
-        .bg-primary-100 {
-          background-color: ${primaryColor}10;
-        }
-        .border-primary {
-          border-color: ${primaryColor};
-        }
-      `}</style>
-    </div>
-  )
+      <Footer />
+    </Editmode>
+  );
 }
 
-export default Layout
+export default Layout;
--- themes/dublin/pages/index.js
+++ themes/dublin/pages/index.js
@@ -6,32 +6,22 @@ import Faq from "../sections/FAQ";
 import Testimonials from "../sections/Testimonials";
 import UserBrands from "../sections/UserBrands";
 import Footer from "../sections/Footer";
-import { useEffect } from "react";
-import handleNav from "../utils/handleNav";
-import { Editmode } from 'editmode-react'
 import Layout from '../components/Layout'
 
 
 export default function Home() {
-  useEffect(() => {
-    handleNav();
-  }, []);
   return (
     <div>
-      <TopNavbar/>
       <Hero />
       <HowItWorks />
       <Features />
       <Faq />
       <Testimonials />
       <UserBrands />
-      <Footer />
     </div>
   );
 }
 
 Home.getLayout = (page) => (
-  <Editmode projectId={process.env.NEXT_PUBLIC_PROJECT_ID}>
     <Layout>{page}</Layout>
-  </Editmode>
 );
