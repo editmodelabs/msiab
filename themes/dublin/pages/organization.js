b575af5378697cd2c972826216432695e2c5ddc2
tonyennis145 <ennis.tony@gmail.com>
2021-07-16 14:21:22 +0800

    Clean up styles on some pages

--- themes/dublin/pages/organization.js
+++ themes/dublin/pages/organization.js
@@ -3,12 +3,13 @@ import Layout from "../components/Layout";
 
 export default function Organization() {
   return (
-    <section className="pb-12 lg:pb-24 pt-12 lg:pt-20 overflow-hidden bg-gray-100 min-h-screen">
+    <section className="pb-12 lg:pb-24 pt-12 lg:pt-20 overflow-hidden bg-gray-100">
+
+      <h1 className="text-xl lg:text-3xl font-bold mb-10 text-center">
+        <Chunk contentKey="organization_static_page" field="Title"/>
+      </h1>
       <div className="container px-4 mx-auto">
-        <div className="rounded bg-white shadow px-12 py-10 text-center">
-          <h1 className="text-xl lg:text-3xl font-bold mb-10">
-            <Chunk contentKey="organization_static_page" field="Title"/>
-          </h1>
+        <div className="rounded bg-white shadow px-12 py-10">
           <div>
             <Chunk contentKey="organization_static_page" field="Body"/>
           </div>
