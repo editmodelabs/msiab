f0b806f3b7f6ed41fee99796821d6ad37da20fe6
Megan Ennis <mennis0145@gmail.com>
2021-07-22 10:35:50 +0100

    Remove redundant border on top of first Job Posting

--- themes/dublin/sections/JobPostings.jsx
+++ themes/dublin/sections/JobPostings.jsx
@@ -5,7 +5,7 @@ export default function JobPostings() {
   return (
     <section className="relative py-20 2xl:py-40 bg-gray-800 border-t border-gray-700">
       <div className="container px-4 mx-auto">
-        <div className="mb-14 pb-16 text-center border-b border-gray-500">
+        <div className="mb-14 pb-16 text-center">
           <span className="text-lg text-blue-500 font-bold">
             <Chunk contentKey="job_postings" field="Tagline" />
           </span>
