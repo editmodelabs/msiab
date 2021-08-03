624b01263602ea093aa83c2f0ab32360e6c2bbe7
Francis Bulus <notchera@gmail.com>
2021-08-03 21:40:39 +0100

    working without purge for now

--- themes/dublin/pages/_app.js
+++ themes/dublin/pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-// import "../styles/custom.css";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
--- themes/dublin/tailwind.config.js
+++ themes/dublin/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
