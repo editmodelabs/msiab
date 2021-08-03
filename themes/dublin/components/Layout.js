9386d6ee926f232717600bf5065042b728e9a7b2
Francis Bulus <notchera@gmail.com>
2021-08-03 16:31:10 +0100

    add custom head to layout

--- themes/dublin/components/Layout.js
+++ themes/dublin/components/Layout.js
@@ -1,16 +1,12 @@
-import Head from "next/head";
 import Styles from "./Styles";
 import TopNavbar from "../sections/TopNavbar";
 import Footer from "../sections/Footer";
-import { useGetChunk, Editmode } from "editmode-react";
+import { Editmode } from "editmode-react";
 import handleNav from "../utils/handleNav";
 import { useEffect } from "react";
+import CustomHead from "./CustomHead";
 
 function Layout({ children }) {
-  const defaultMetaTitle = `${useGetChunk("company_name")} -  ${useGetChunk(
-    "company_tagline"
-  )}`;
-  const font_composition = useGetChunk("google_font_family_composition");
   useEffect(() => {
     handleNav();
   }, []);
@@ -22,14 +18,7 @@ function Layout({ children }) {
       defaultChunks={chunks}
       next={true}
     >
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>{defaultMetaTitle}</title>
-        <link
-          href={`https://fonts.googleapis.com/css2?family=${font_composition}&display=swap`}
-          rel="stylesheet"
-        />
-      </Head>
+      <CustomHead />
       <Styles />
       <TopNavbar />
       {children}
