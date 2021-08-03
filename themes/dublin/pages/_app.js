66c846fa63d454e39be5b122749a6f8085202504
Francis Bulus <notchera@gmail.com>
2021-08-03 21:06:08 +0100

    testing different post css config

--- themes/dublin/pages/_app.js
+++ themes/dublin/pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
-import "../styles/custom.css";
+// import "../styles/custom.css";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
 }
 
-export default MyApp
+export default MyApp;
--- themes/dublin/utils/computeClassName.js
+++ themes/dublin/utils/computeClassName.js
@@ -1,12 +1,6 @@
 export default function computeClassName(isActive) {
-  let className;
   if (isActive) {
-    className +=
-      " px-4 py-4 w-full md:w-1/2 lg:w-auto font-bold text-primary border-b-2 border-primary";
+    return " px-4 py-4 w-full md:w-1/2 lg:w-auto font-bold text-primary border-b-2 border-primary";
   } else
-    className =
-      className +
-      " px-4 py-4 w-full md:w-1/2 lg:w-auto font-bold text-gray-400 hover:text-primary border-b-2 border-transparent transition duration-150";
-
-  return className;
+    return " px-4 py-4 w-full md:w-1/2 lg:w-auto font-bold text-gray-400 hover:text-primary border-b-2 border-transparent transition duration-150";
 }
