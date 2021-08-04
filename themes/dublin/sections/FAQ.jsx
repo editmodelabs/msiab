a16595c2adac55faa557a304ced15ed9ce22d86c
Megan Ennis <mennis0145@gmail.com>
2021-08-04 15:27:36 +0100

    Replace homepage FAQs headline and tagline with Headline Set collection items - as opposed to standalone chunks.

--- themes/dublin/sections/FAQ.jsx
+++ themes/dublin/sections/FAQ.jsx
@@ -7,8 +7,8 @@ export default function Faqs() {
     <section className="py-20 2xl:py-40 bg-blue-50">
       <div className="container px-4 mx-auto">
         <div className="mb-20 text-center">
-          <span className="text-lg font-bold text-blue-500"><Chunk contentKey="faq_section_heading_before"/></span>
-          <h2 className="mt-8 text-7xl font-bold font-heading"><Chunk contentKey="faq_section_heading"/></h2>
+          <span className="text-lg font-bold text-blue-500"><Chunk contentKey="faq_home_section" field="Tagline"/></span>
+          <h2 className="mt-8 text-7xl font-bold font-heading"><Chunk contentKey="faq_home_section" field="Headline"/></h2>
         </div>
         <div className="max-w-4xl mx-auto">
           <FaqTabs />
