84b52a35cf6b5d42913734233b28c537bc8d9b7c
Megan Ennis <mennis0145@gmail.com>
2021-08-04 15:38:04 +0100

    Replace homepage UserBrands headline and tagline with Headline Set collection items.

--- themes/dublin/sections/UserBrands.jsx
+++ themes/dublin/sections/UserBrands.jsx
@@ -7,8 +7,12 @@ export default function UserBrands() {
         <div className="flex flex-wrap items-center -mx-4">
           <div className="w-full lg:w-2/5 px-4 mb-20 lg:mb-0">
             <div className="lg:max-w-lg">
-              <h2 className="mb-12 text-6xl 2xl:text-7xl font-bold font-heading text-white"><Chunk contentKey="brand_statement"/></h2>
-              <p className="text-lg text-gray-200"><Chunk contentKey="brand_statement_desc"/></p>
+              <h2 className="mb-12 text-6xl 2xl:text-7xl font-bold font-heading text-white">
+                <Chunk contentKey="brands_home_section" field="Headline"/>
+              </h2>
+              <p className="text-lg text-gray-200">
+                <Chunk contentKey="brands_home_section" field="Description"/>
+              </p>
             </div>
           </div>
           <div className="w-full lg:w-3/5 px-4">
